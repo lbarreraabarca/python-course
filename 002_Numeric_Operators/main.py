a53f3a1328e57b3c4cf54a6a321f2b89e2fc76b6
lmbarrera <lmbarrera@falabella.cl>
2022-02-21 11:06:57 -0300

    Added 002_Numeric_Operators script.

---
+++ 002_Numeric_Operators/main.py
@@ -0,0 +1,29 @@
+## Basics operators
+
+# Sum
+x = 2 + 3
+print(x)
+
+# Substract
+y = x - 4
+print(y)
+
+# Multiply
+z = x * 8
+print(z)
+
+# Division
+a = z / 13
+round(a, 3)
+
+# Advanced math functions.
+import math
+
+# Pow function receives two arguments.
+b = math.pow(4,2)
+# Square root function receives one argument.
+c = math.sqrt(b)
+# Cosine function receives one argument.
+print(math.cos(0))
+# Factorial function receives one argument.
+print('factorial 5 : ', math.factorial(5))
